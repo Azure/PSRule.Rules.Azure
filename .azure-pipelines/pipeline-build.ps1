d9b524af8d5fad30010f6029f78e2b76b0a19033
Bernie White <bewhite@microsoft.com>
2019-05-23 23:12:36 +1000

    Install dependencies as a separate task

--- .azure-pipelines/pipeline-build.ps1
+++ .azure-pipelines/pipeline-build.ps1
@@ -34,17 +34,4 @@ if ($Env:SYSTEM_DEBUG -eq 'true') {
     $VerbosePreference = 'Continue';
 }
 
-if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction Ignore)) {
-    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
-}
-
-if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.1.2 -ErrorAction Ignore)) {
-    Install-Module PowerShellGet -MinimumVersion 2.1.2 -Scope CurrentUser -Force -AllowClobber;
-    Import-Module PowerShellGet -MinimumVersion 2.1.2 -Force;
-}
-
-if ($Null -eq (Get-InstalledModule -Name InvokeBuild -MinimumVersion 5.4.0 -ErrorAction Ignore)) {
-    Install-Module InvokeBuild -MinimumVersion 5.4.0 -Scope CurrentUser -Force;
-}
-
 Invoke-Build @PSBoundParameters
