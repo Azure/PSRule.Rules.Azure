c0d08c876df56d6d61d3df39786e3555c7c7746f
Bernie White <bewhite@microsoft.com>
2019-11-09 00:07:15 +1000

    Updated rules to use type pre-conditions #144 (#146)

--- src/PSRule.Rules.Azure/rules/Azure.PublicIP.Rule.ps1
+++ src/PSRule.Rules.Azure/rules/Azure.PublicIP.Rule.ps1
@@ -3,7 +3,7 @@
 #
 
 # Synopsis: Public IP addresses should be attached or cleaned up if not in use
-Rule 'Azure.PublicIP.IsAttached' -If { ResourceType 'Microsoft.Network/publicIPAddresses' } -Tag @{ severity = 'Awareness'; category = 'Operations management' } {
+Rule 'Azure.PublicIP.IsAttached' -Type 'Microsoft.Network/publicIPAddresses' -Tag @{ severity = 'Awareness'; category = 'Operations management' } {
     Recommend 'Public IP address should be attached'
 
     ($Null -ne $TargetObject.Properties.ipConfiguration) -and
