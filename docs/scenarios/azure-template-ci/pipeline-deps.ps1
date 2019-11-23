32e5d4434cfeeaae7273e2d148851296eb638d44
Bernie White <bewhite@microsoft.com>
2019-11-23 12:19:42 +1000

    Add CI template export scenario #145

---
+++ docs/scenarios/azure-template-ci/pipeline-deps.ps1
@@ -0,0 +1,11 @@
+#
+# Install dependencies
+#
+
+if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction Ignore)) {
+    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
+}
+
+if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.2.1 -ErrorAction Ignore)) {
+    Install-Module PowerShellGet -MinimumVersion 2.2.1 -Scope CurrentUser -Force -AllowClobber;
+}
