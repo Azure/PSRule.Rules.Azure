ea17a02fc004674b1bc00c4a5016770c7ff1a33e
Bernie White <bewhite@microsoft.com>
2020-02-05 23:56:02 +1100

    Set required version to PSRule v0.13.0

--- pipeline.build.ps1
+++ pipeline.build.ps1
@@ -194,8 +194,8 @@ task PSScriptAnalyzer NuGet, {
 
 # Synopsis: Install PSRule
 task PSRule NuGet, {
-    if ($Null -eq (Get-InstalledModule -Name PSRule -MinimumVersion 0.13.0 -ErrorAction Ignore)) {
-        Install-Module -Name PSRule -Repository PSGallery -MinimumVersion 0.13.0 -Scope CurrentUser -Force;
+    if ($Null -eq (Get-InstalledModule -Name PSRule -RequiredVersion 0.13.0 -ErrorAction Ignore)) {
+        Install-Module -Name PSRule -Repository PSGallery -RequiredVersion 0.13.0 -Scope CurrentUser -Force;
     }
     Import-Module -Name PSRule -Verbose:$False;
 }
