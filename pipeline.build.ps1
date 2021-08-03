381943580b60905aa56f08e7651569ef5b2056af
Bernie White <bewhite@microsoft.com>
2021-08-04 02:37:43 +1000

    Update pipeline

--- pipeline.build.ps1
+++ pipeline.build.ps1
@@ -260,7 +260,7 @@ task CopyModule MinifyData, {
 # Synopsis: Build modules only
 task BuildModule BuildDotNet, CopyModule
 
-task TestModule ModuleDependencies, Pester, PSScriptAnalyzer, InstallBicep, {
+task TestModule ModuleDependencies, Pester, PSScriptAnalyzer, {
     # Run Pester tests
     $pesterParams = @{ Path = (Join-Path -Path $PWD -ChildPath tests/PSRule.Rules.Azure.Tests); OutputFile = 'reports/pester-unit.xml'; OutputFormat = 'NUnitXml'; PesterOption = @{ IncludeVSCodeMarker = $True }; PassThru = $True; };
 
@@ -423,49 +423,6 @@ task ExportProviders {
         $Null = New-Item -Path $dataPath -ItemType Directory -Force;
     }
     $index | ConvertTo-Json -Depth 20 | Set-Content ./data/providers.json;
-}
-
-task InstallBicep {
-    $bicepCmd = Get-Command bicep -ErrorAction SilentlyContinue;
-    $bicepCmd
-    if ($Null -eq $bicepCmd) {
-        # Linux
-        if ($IsLinux) {
-             # Fetch the latest Bicep CLI binary
-            (New-Object Net.WebClient).DownloadFile('https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64', "$PWD/bicep.bin")
-            exec {
-                # Mark it as executable
-                chmod +x ./bicep.bin
-                # Add bicep to your PATH (requires admin)
-                mv ./bicep.bin /usr/local/bin/bicep
-            }
-        }
-        elseif ($IsMacOS) {
-            (New-Object Net.WebClient).DownloadFile('https://github.com/Azure/bicep/releases/latest/download/bicep-osx-x64', "$PWD/bicep")
-            exec {
-                # Mark it as executable
-                chmod +x ./bicep
-                # Add Gatekeeper exception (requires admin)
-                sudo spctl --add ./bicep
-                # Add bicep to your PATH (requires admin)
-                sudo mv ./bicep /usr/local/bin/bicep
-            }
-        }
-        else {
-            # Create the install folder
-            $installPath = "$env:USERPROFILE\.bicep"
-            $installDir = New-Item -ItemType Directory -Path $installPath -Force
-            $installDir.Attributes += 'Hidden'
-            # Fetch the latest Bicep CLI binary
-            (New-Object Net.WebClient).DownloadFile("https://github.com/Azure/bicep/releases/latest/download/bicep-win-x64.exe", "$installPath\bicep.exe")
-            # Add bicep to your PATH
-            $currentPath = (Get-Item -path "HKCU:\Environment" ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
-            if (-not $currentPath.Contains("%USERPROFILE%\.bicep")) { setx PATH ($currentPath + ";%USERPROFILE%\.bicep") }
-            if (-not $env:path.Contains($installPath)) { $env:path += ";$installPath" }
-        }
-
-        bicep --version
-    }
 }
 
 task ExportData ExportProviders
