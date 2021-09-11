194ba27899c86de0d7996789ca4e03374b6eb5d6
Bernie White <bewhite@microsoft.com>
2021-09-11 16:40:29 +1000

    Refactor baseline doc generate task (#950)

--- BaselineToc.Doc.ps1
+++ BaselineToc.Doc.ps1
@@ -1,9 +1,16 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+Export-PSDocumentConvention 'NameBaseline' -Process {
+    $PSDocs.Document.InstanceName = $PSDocs.TargetObject.Name;
+}
+
 Document 'baseline' {
-    $baselineName = $InputObject.Name;
-    $obsolete = $InputObject.metadata.annotations.obsolete -eq $True;
+    $baselineName = $PSDocs.TargetObject.Name;
+    $obsolete = $PSDocs.TargetObject.metadata.annotations.obsolete -eq $True;
+
+    Write-Verbose -Message "[Baseline] -- Processing baseline: $baselineName";
+    Write-Verbose -Message "[Baseline] -- Baseline is obsolete: $obsolete";
 
     Title $baselineName;
 
@@ -11,16 +18,16 @@ Document 'baseline' {
         'Obsolete' | BlockQuote
     }
 
-    Import-Module ./out/modules/PSRule.Rules.Azure;
-    $rules = @(Get-PSRule -Module PSRule.Rules.Azure -Baseline $baselineName -WarningAction SilentlyContinue);
+    $rules = $PSDocs.TargetObject.Rules | Sort-Object -Property RuleName;
     $ruleCount = $rules.Length;
 
-    $InputObject.Synopsis;
+    $PSDocs.TargetObject.Synopsis;
 
-    Section 'Rules' {
-        "The following rules are included within ``$baselineName``. This baseline includes a total of $ruleCount rules."
+    Write-Verbose -Message "[Baseline] -- Found $ruleCount rules.";
 
-        $rules | Sort-Object -Property RuleName | Table -Property @{ Name = 'Name'; Expression = {
+    Section 'Rules' -If { $ruleCount -gt 0 } {
+        "The following rules are included within ``$baselineName``. This baseline includes a total of $ruleCount rules.";
+        $rules | Table -Property @{ Name = 'Name'; Expression = {
             "[$($_.RuleName)](../rules/$($_.RuleName).md)"
         }}, Synopsis, @{ Name = 'Severity'; Expression = {
             $_.Info.Annotations.severity
