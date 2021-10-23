9885c762aa0b0f1fd42c9b6f05c35bdff8cda4d9
Bernie White <bewhite@microsoft.com>
2021-10-23 12:43:19 +1000

    Install Bicep for PSRule GitHub Action #1050 (#1051)

--- src/PSRule.Rules.Azure/rules/Conventions.Rule.ps1
+++ src/PSRule.Rules.Azure/rules/Conventions.Rule.ps1
@@ -52,4 +52,20 @@ Export-PSRuleConvention 'Azure.ExpandBicep' -If { $Configuration.AZURE_BICEP_FIL
     }
 }
 
+$Global:InstalledBicep = $False;
+
+# Synopsis: Install Bicep for expansion of .bicep files within GitHub Actions.
+Export-PSRuleConvention 'Azure.BicepInstall' -If { !$InstalledBicep -and $Configuration.AZURE_BICEP_FILE_EXPANSION -eq $True -and $Env:GITHUB_ACTION -eq '__Microsoft_ps-rule' } -Begin {
+    # Install the latest Bicep CLI binary for alpine
+    Invoke-WebRequest -Uri 'https://github.com/Azure/bicep/releases/latest/download/bicep-linux-musl-x64' -OutFile $Env:GITHUB_WORKSPACE/bicep.bin
+
+    # Set executable
+    chmod +x $Env:GITHUB_WORKSPACE/bicep.bin
+
+    # Copy to PATH environment
+    Move-Item $Env:GITHUB_WORKSPACE/bicep.bin /usr/local/bin/bicep
+
+    $Global:InstalledBicep = $True;
+}
+
 #endregion Bicep
