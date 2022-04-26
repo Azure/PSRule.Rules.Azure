554be9583bb8c561813bcdc6d8f07d84e8c2bcf7
Bernie White <bewhite@microsoft.com>
2022-04-27 00:10:54 +1000

    Fix dependency version replacement

--- pipeline.build.ps1
+++ pipeline.build.ps1
@@ -122,10 +122,10 @@ task VersionModule ModuleDependencies, {
         if ($_.Name -eq 'PSRule' -and $Configuration -eq 'Release') {
             @{ ModuleName = 'PSRule'; ModuleVersion = $dependencies.dependencies.PSRule.version }
         }
-        if ($_.Name -eq 'Az.Accounts' -and $Configuration -eq 'Release') {
+        elseif ($_.Name -eq 'Az.Accounts' -and $Configuration -eq 'Release') {
             @{ ModuleName = 'Az.Accounts'; ModuleVersion = $dependencies.dependencies.'Az.Accounts'.version }
         }
-        if ($_.Name -eq 'Az.Resources' -and $Configuration -eq 'Release') {
+        elseif ($_.Name -eq 'Az.Resources' -and $Configuration -eq 'Release') {
             @{ ModuleName = 'Az.Resources'; ModuleVersion = $dependencies.dependencies.'Az.Resources'.version }
         }
         else {
