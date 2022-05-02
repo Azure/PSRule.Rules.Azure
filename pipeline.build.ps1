d8e1ca13b86bbe3fefee42665326907f553f6278
Bernie White <bewhite@microsoft.com>
2022-05-02 23:16:05 +1000

    Add pre-build

--- pipeline.build.ps1
+++ pipeline.build.ps1
@@ -162,6 +162,7 @@ task ModuleDependencies Dependencies, {
 task BuildDotNet {
     exec {
         # Build library
+        dotnet build
         dotnet publish src/PSRule.Rules.Azure -c $Configuration -f netstandard2.0 -o $(Join-Path -Path $PWD -ChildPath out/modules/PSRule.Rules.Azure) -p:version=$Build
     }
 }
