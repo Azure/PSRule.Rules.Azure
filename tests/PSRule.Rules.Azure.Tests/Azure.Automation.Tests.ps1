8beaeeada8e188b3a9d1fd04c6fe2127b7d552d8
Benjamin Engeset <beneng@hotmail.no>
2022-10-03 10:40:21 +0200

    Updated typo in testcases

--- tests/PSRule.Rules.Azure.Tests/Azure.Automation.Tests.ps1
+++ tests/PSRule.Rules.Azure.Tests/Azure.Automation.Tests.ps1
@@ -92,9 +92,9 @@ Describe 'Azure.Automation' -Tag Automation {
             $ruleResult.Length | Should -Be 3;
             $ruleResult.TargetName | Should -Be 'automation-e', 'automation-f', 'automation-g';
 
-            $ruleResult[0].Reason | Should -BeExactly "Minimum one diagnostic setting should have (AuditEvent) configured or category group (audit, allLogs) configured";
-            $ruleResult[1].Reason | Should -BeExactly "Minimum one diagnostic setting should have (AuditEvent) configured or category group (audit, allLogs) configured";
-            $ruleResult[2].Reason | Should -BeExactly "Minimum one diagnostic setting should have (AuditEvent) configured or category group (audit, allLogs) configured";
+            $ruleResult[0].Reason | Should -BeExactly "Minimum one diagnostic setting should have (AuditEvent) configured or category group (audit, allLogs) configured.";
+            $ruleResult[1].Reason | Should -BeExactly "Minimum one diagnostic setting should have (AuditEvent) configured or category group (audit, allLogs) configured.";
+            $ruleResult[2].Reason | Should -BeExactly "Minimum one diagnostic setting should have (AuditEvent) configured or category group (audit, allLogs) configured.";
 
             # Pass
             $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Pass' });
