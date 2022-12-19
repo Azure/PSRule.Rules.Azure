3bda24e724249f3000c3dfa1f5db893617ae68a1
Bernie White <bewhite@microsoft.com>
2022-12-20 01:05:17 +1000

    Add supports tag selector for indexed mode #1946 (#1970)

---
+++ src/PSRule.Rules.Azure/Data/Policy/PolicyMode.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace PSRule.Rules.Azure.Data.Policy
+{
+    /// <summary>
+    /// A list of supported policy modes.
+    /// </summary>
+    internal enum PolicyMode
+    {
+        None = 0,
+
+        All,
+
+        Indexed
+    }
+}
