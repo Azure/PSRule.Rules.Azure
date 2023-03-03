399c5421a7fcb0785f935f8621e7e4a500d19cfe
Dimitri Zilber <zilberd@users.noreply.github.com>
2023-03-03 08:04:47 +0100

    Update tests/PSRule.Rules.Azure.Tests/Azure.KeyVault.Tests.ps1
    Co-authored-by: Bernie White <bewhite@microsoft.com>

--- tests/PSRule.Rules.Azure.Tests/Azure.KeyVault.Tests.ps1
+++ tests/PSRule.Rules.Azure.Tests/Azure.KeyVault.Tests.ps1
@@ -360,13 +360,14 @@ Describe 'Azure.KeyVault' -Tag 'KeyVault' {
             $filteredResult = $result | Where-Object { $_.RuleName -eq 'Azure.KeyVault.Firewall' };
 
             # Fails
+            $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Fail' });
+            $ruleResult | Should -Not -BeNullOrEmpty;
+            $ruleResult.Length | Should -Be 1;
+            $ruleResult.TargetName | Should -Be 'keyvault-001';
+            
+            # No passes
             $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Pass' });
             $ruleResult | Should -BeNullOrEmpty;
-#            test should be fail when the module will be published            
-#            $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Fail' });
-#            $ruleResult | Should -Not -BeNullOrEmpty;
-#            $ruleResult.Length | Should -Be 1;
-#            $ruleResult.TargetName | Should -Be 'keyvault-001';
         }
     }
 }
