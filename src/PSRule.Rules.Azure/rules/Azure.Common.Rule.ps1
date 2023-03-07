aa7415e5c57fafa22c792ef616693767f0b15713
Benjamin Engeset <beneng@hotmail.no>
2023-03-07 11:32:00 +0100

    style: :art: Formatter fix

--- src/PSRule.Rules.Azure/rules/Azure.Common.Rule.ps1
+++ src/PSRule.Rules.Azure/rules/Azure.Common.Rule.ps1
@@ -537,9 +537,9 @@ function global:HasOMSOrAMAExtension {
         }
         elseif ($PSRule.TargetType -eq 'Microsoft.Compute/virtualMachineScaleSets') {
             $property = $TargetObject.Properties.virtualMachineProfile.extensionProfile.extensions.properties |
-            Where-Object { ($_.publisher -eq 'Microsoft.EnterpriseCloud.Monitoring') -or ($_.publisher -eq 'Microsoft.Azure.Monitor') }
-            $subresource = @(GetSubResources -ResourceType 'Microsoft.Compute/virtualMachineScaleSets/extensions' |
-                Where-Object { ($_.Properties.publisher -eq 'Microsoft.EnterpriseCloud.Monitoring') -or ($_.Properties.publisher -eq 'Microsoft.Azure.Monitor') })
+                Where-Object { ($_.publisher -eq 'Microsoft.EnterpriseCloud.Monitoring') -or ($_.publisher -eq 'Microsoft.Azure.Monitor') }
+                    $subresource = @(GetSubResources -ResourceType 'Microsoft.Compute/virtualMachineScaleSets/extensions' |
+                        Where-Object { ($_.Properties.publisher -eq 'Microsoft.EnterpriseCloud.Monitoring') -or ($_.Properties.publisher -eq 'Microsoft.Azure.Monitor') })
                       
             $extensions = @($property; $subresource)
             $Assert.Greater($extensions, '.', 0).Result
