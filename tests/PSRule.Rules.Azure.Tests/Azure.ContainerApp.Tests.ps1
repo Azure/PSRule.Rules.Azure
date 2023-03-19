563bfb44e99655183446d7438ce1b2e42677fdfb
Benjamin Engeset <beneng@hotmail.no>
2023-03-19 10:50:37 +0100

    Updated tests

--- tests/PSRule.Rules.Azure.Tests/Azure.ContainerApp.Tests.ps1
+++ tests/PSRule.Rules.Azure.Tests/Azure.ContainerApp.Tests.ps1
@@ -43,14 +43,14 @@ Describe 'Azure.ContainerApp' -Tag 'ContainerApp' {
             # Fail
             $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Fail' });
             $ruleResult | Should -Not -BeNullOrEmpty;
-            $ruleResult.Length | Should -Be 1;
-            $ruleResult.TargetName | Should -Be 'capp-B';
+            $ruleResult.Length | Should -Be 2;
+            $ruleResult.TargetName | Should -BeIn 'capp-B', 'capp-D';
 
             # Pass
             $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Pass' });
             $ruleResult | Should -Not -BeNullOrEmpty;
-            $ruleResult.Length | Should -Be 1;
-            $ruleResult.TargetName | Should -BeIn 'capp-A';
+            $ruleResult.Length | Should -Be 2;
+            $ruleResult.TargetName | Should -BeIn 'capp-A', 'capp-C';
         }
 
         It 'Azure.ContainerApp.ManagedIdentity' {
