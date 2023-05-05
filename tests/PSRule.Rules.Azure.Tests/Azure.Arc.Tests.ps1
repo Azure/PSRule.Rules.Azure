cb5cbb412d77a579099de468723a8a49cd79b70d
Benjamin Engeset <99641908+BenjaminEngeset@users.noreply.github.com>
2023-05-05 17:26:35 +0200

    Added Azure.Arc.Kubernetes.Defender (#2182)
    * Added Azure.Arc.Kubernetes.Defender
    
    * Fix changelog
    
    * Added label
    
    Co-authored-by: Bernie White <bewhite@microsoft.com>
    
    * Added reference
    
    Co-authored-by: Bernie White <bewhite@microsoft.com>
    
    ---------
    
    Co-authored-by: Bernie White <bewhite@microsoft.com>

---
+++ tests/PSRule.Rules.Azure.Tests/Azure.Arc.Tests.ps1
@@ -0,0 +1,57 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+#
+# Unit tests for Azure Arc rules
+#
+
+[CmdletBinding()]
+param ()
+
+BeforeAll {
+    # Setup error handling
+    $ErrorActionPreference = 'Stop';
+    Set-StrictMode -Version latest;
+
+    if ($Env:SYSTEM_DEBUG -eq 'true') {
+        $VerbosePreference = 'Continue';
+    }
+
+    # Setup tests paths
+    $rootPath = $PWD;
+    Import-Module (Join-Path -Path $rootPath -ChildPath out/modules/PSRule.Rules.Azure) -Force;
+    $here = (Resolve-Path $PSScriptRoot).Path;
+}
+
+Describe 'Azure.Arc' -Tag 'Arc' {
+    Context 'Conditions' {
+        BeforeAll {
+            $invokeParams = @{
+                Baseline      = 'Azure.All'
+                Module        = 'PSRule.Rules.Azure'
+                WarningAction = 'Ignore'
+                ErrorAction   = 'Stop'
+            }
+            $dataPath = Join-Path -Path $here -ChildPath 'Resources.Arc.json';
+            $result = Invoke-PSRule @invokeParams -InputPath $dataPath;
+        }
+
+        It 'Azure.Arc.Kubernetes.Defender' {
+            $filteredResult = $result | Where-Object { $_.RuleName -eq 'Azure.Arc.Kubernetes.Defender' };
+            
+            # Fail
+            $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Fail' });
+            $ruleResult | Should -Not -BeNullOrEmpty;
+            $ruleResult.Length | Should -Be 1;
+            $ruleResult.TargetName | Should -BeIn 'cluster-A';
+
+            $ruleResult[0].Reason | Should -BeExactly "The Arc-enabled Kubernetes cluster 'cluster-A' should have a Microsoft Defender for Containers extension configured.";
+
+            # Pass
+            $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Pass' });
+            $ruleResult | Should -Not -BeNullOrEmpty;
+            $ruleResult.Length | Should -Be 1;
+            $ruleResult.TargetName | Should -BeIn 'cluster-B';
+        }
+    }
+}
