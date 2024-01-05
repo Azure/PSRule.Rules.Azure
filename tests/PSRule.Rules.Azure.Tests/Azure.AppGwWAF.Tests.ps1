a59f33e016a954908240faab2096136e125a6e5c
Benjamin Engeset <benjaminengeset@outlook.com>
2024-01-05 11:46:30 +0100

    feat: Updated  Azure.AppGwWAF.RuleGroups

--- tests/PSRule.Rules.Azure.Tests/Azure.AppGwWAF.Tests.ps1
+++ tests/PSRule.Rules.Azure.Tests/Azure.AppGwWAF.Tests.ps1
@@ -87,13 +87,11 @@ Describe 'Azure.AppGWWAF' -Tag 'Network', 'AppGwWAF' {
 
             # Fail
             $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Fail' });
-            $ruleResult | Should -Not -BeNullOrEmpty;
             $ruleResult.Length | Should -Be 2;
             $ruleResult.TargetName | Should -Be 'appgwwaf-A', 'appgwwaf-B';
 
             # Pass
             $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Pass' });
-            $ruleResult | Should -Not -BeNullOrEmpty;
             $ruleResult.Length | Should -Be 1;
             $ruleResult.TargetName | Should -Be 'appgwwaf-C';
         }
--- tests/PSRule.Rules.Azure.Tests/Azure.Baseline.Tests.ps1
+++ tests/PSRule.Rules.Azure.Tests/Azure.Baseline.Tests.ps1
@@ -206,7 +206,7 @@ Describe 'Baselines' -Tag Baseline {
             $result = @(Get-PSRule -Module PSRule.Rules.Azure -Baseline 'Azure.GA_2023_09' -WarningAction Ignore);
             $filteredResult = @($result | Where-Object { $_.Tag.release -in 'GA'});
             $filteredResult | Should -Not -BeNullOrEmpty;
-            $filteredResult.Length | Should -Be 387;
+            $filteredResult.Length | Should -Be 386;
         }
 
         It 'With Azure.Preview_2023_09' {
@@ -220,7 +220,7 @@ Describe 'Baselines' -Tag Baseline {
             $result = @(Get-PSRule -Module PSRule.Rules.Azure -Baseline 'Azure.GA_2023_12' -WarningAction Ignore);
             $filteredResult = @($result | Where-Object { $_.Tag.release -in 'GA'});
             $filteredResult | Should -Not -BeNullOrEmpty;
-            $filteredResult.Length | Should -Be 396;
+            $filteredResult.Length | Should -Be 395;
         }
 
         It 'With Azure.Preview_2023_12' {
