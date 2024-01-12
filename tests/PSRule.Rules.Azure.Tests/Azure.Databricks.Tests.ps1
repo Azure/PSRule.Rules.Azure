a682f5eda3d9ca73559bad952b602f640c96c45c
batemansogq <bateman_so_gq@hotmail.com>
2024-01-12 02:24:51 +0000

    added the ADB SKU rule

--- tests/PSRule.Rules.Azure.Tests/Azure.Databricks.Tests.ps1
+++ tests/PSRule.Rules.Azure.Tests/Azure.Databricks.Tests.ps1
@@ -51,5 +51,21 @@ Describe 'Azure.Databricks' -Tag 'databricks' {
             $ruleResult.Length | Should -Be 1;
             $ruleResult.TargetName | Should -Be 'databricks-A';
         }
+
+        It 'Azure.Databricks.SKU' {
+            $filteredResult = $result | Where-Object { $_.RuleName -eq 'Azure.Databricks.SKU' };
+
+            # Fail
+            $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Fail' });
+            $ruleResult | Should -Not -BeNullOrEmpty;
+            $ruleResult.Length | Should -Be 1;
+            $ruleResult.TargetName | Should -Be 'databricks-B';
+
+            # Pass
+            $ruleResult = @($filteredResult | Where-Object { $_.Outcome -eq 'Pass' });
+            $ruleResult | Should -Not -BeNullOrEmpty;
+            $ruleResult.Length | Should -Be 1;
+            $ruleResult.TargetName | Should -Be 'databricks-A';
+        }
     }
 }
