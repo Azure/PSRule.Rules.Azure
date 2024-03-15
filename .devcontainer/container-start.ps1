d1573d282b75afbf126213a1bfec752b99d28ecb
Bernie White <bewhite@microsoft.com>
2024-03-15 15:34:09 +0000

    Copy bicep in dev container

--- .devcontainer/container-start.ps1
+++ .devcontainer/container-start.ps1
@@ -9,3 +9,13 @@ pip install -r requirements-docs.txt
 
 # Restore .NET packages
 dotnet restore
+
+# Fetch the latest Bicep CLI binary
+curl -Lo bicep https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
+# Mark it as executable
+chmod +x ./bicep
+# Add bicep to your PATH (requires admin)
+sudo mv ./bicep /usr/local/bin/bicep
+# Verify you can now access the 'bicep' command
+bicep --version
+# Done!
