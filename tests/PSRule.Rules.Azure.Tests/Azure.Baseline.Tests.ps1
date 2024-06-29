76496a92513f25bd06fdc6363533a6b362bedc8a
Bernie White <bewhite@microsoft.com>
2024-06-29 23:51:41 +1000

    Add June 2024 baselines

--- tests/PSRule.Rules.Azure.Tests/Azure.Baseline.Tests.ps1
+++ tests/PSRule.Rules.Azure.Tests/Azure.Baseline.Tests.ps1
@@ -243,5 +243,19 @@ Describe 'Baselines' -Tag Baseline {
             $filteredResult | Should -Not -BeNullOrEmpty;
             $filteredResult.Length | Should -Be 8;
         }
+
+        It 'With Azure.GA_2024_06' {
+            $result = @(Get-PSRule -Module PSRule.Rules.Azure -Baseline 'Azure.GA_2024_06' -WarningAction Ignore);
+            $filteredResult = @($result | Where-Object { $_.Tag.release -in 'GA'});
+            $filteredResult | Should -Not -BeNullOrEmpty;
+            $filteredResult.Length | Should -Be 396;
+        }
+
+        It 'With Azure.Preview_2024_06' {
+            $result = @(Get-PSRule -Module PSRule.Rules.Azure -Baseline 'Azure.Preview_2024_06' -WarningAction Ignore);
+            $filteredResult = @($result | Where-Object { $_.Tag.release -in 'preview'});
+            $filteredResult | Should -Not -BeNullOrEmpty;
+            $filteredResult.Length | Should -Be 8;
+        }
     }
 }
