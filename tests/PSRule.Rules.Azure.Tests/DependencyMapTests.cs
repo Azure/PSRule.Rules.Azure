7d5362479127dd60a665759969857135574b6b19
Bernie White <bewhite@microsoft.com>
2025-02-19 09:00:31 +1000

    Fixes ordering of symbolic copy loop dependencies #3257 (#3259)

--- tests/PSRule.Rules.Azure.Tests/DependencyMapTests.cs
+++ tests/PSRule.Rules.Azure.Tests/DependencyMapTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Linq;
 using Newtonsoft.Json.Linq;
 using PSRule.Rules.Azure.Data.Template;
 using static PSRule.Rules.Azure.Data.Template.TemplateVisitor;
@@ -10,7 +11,7 @@ namespace PSRule.Rules.Azure
     public sealed class DependencyMapTests
     {
         [Fact]
-        public void SortWithComparer()
+        public void SortDependencies_WithoutSymbolicName_ShouldReorderResources()
         {
             var context = new TemplateContext();
             var resources = new IResourceValue[]
@@ -36,13 +37,13 @@ namespace PSRule.Rules.Azure
 
             // https://github.com/Azure/PSRule.Rules.Azure/issues/2255
             context = new TemplateContext();
-            resources = new IResourceValue[]
-            {
+            resources =
+            [
                 GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/virtualNetworks\", \"name\": \"vnet-001\", \"dependsOn\": [ \"/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/ps-rule-test-rg/providers/Microsoft.Network/routeTables/rt-002\" ] }")),
                 GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/routeTables\", \"name\": \"rt-001\", \"dependsOn\": [ ] }")),
                 GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/routeTables\", \"name\": \"rt-002\" }")),
                 GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/virtualNetworks/subnets\", \"name\": \"vnet-001/subnet-001\", \"dependsOn\": [ \"/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/ps-rule-test-rg/providers/Microsoft.Network/virtualNetworks/vnet-001\" ] }")),
-            };
+            ];
             resources = context.SortDependencies(resources);
 
             actual = resources[0];
@@ -59,7 +60,7 @@ namespace PSRule.Rules.Azure
         }
 
         [Fact]
-        public void SortSymbolicNameWithComparer()
+        public void SortDependencies_WithSymbolicName_ShouldReorderResources()
         {
             var context = new TemplateContext();
             var resources = new IResourceValue[]
@@ -69,30 +70,65 @@ namespace PSRule.Rules.Azure
                 GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/routeTables\", \"name\": \"rt-002\" }"), symbolicName: "rt-002"),
                 GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/virtualNetworks/subnets\", \"name\": \"vnet-001/subnet-001\", \"dependsOn\": [ \"vnet-001\" ] }"), symbolicName: "vnet-001/subnet-001"),
             };
-            resources = context.SortDependencies(resources);
 
-            var actual = resources[0];
-            Assert.Equal("rt-002", actual.Value["name"].Value<string>());
+            var actual = context.SortDependencies(resources).Select(r =>
+            {
+                return r.Value["name"].Value<string>();
+            }).ToArray();
 
-            actual = resources[1];
-            Assert.Equal("vnet-001", actual.Value["name"].Value<string>());
+            Assert.Equal(
+            [
+                "rt-002",
+                "vnet-001",
+                "rt-001",
+                "vnet-001/subnet-001"
+            ], actual);
+        }
 
-            actual = resources[2];
-            Assert.Equal("rt-001", actual.Value["name"].Value<string>());
+        /// <summary>
+        /// If a dependency is a copy with a symbolic name all instances of the dependency should be reordered above the dependant resource.
+        /// </summary>
+        [Fact]
+        public void SortDependencies_WithMultipleResourceCopies_ShouldReorderAllResources()
+        {
+            var context = new TemplateContext();
+            var resources = new IResourceValue[]
+            {
+                GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/virtualNetworks\", \"name\": \"vnet-001\", \"dependsOn\": [ \"routeTables\" ] }"), symbolicName: "vnet-001"),
+                GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/routeTables\", \"name\": \"rt-001\", \"dependsOn\": [ ] }"), symbolicName: "routeTables", copyInstance: 0),
+                GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/routeTables\", \"name\": \"rt-002\" }"), symbolicName: "routeTables", copyInstance: 1),
+                GetResourceValue(context, JObject.Parse("{ \"type\": \"Microsoft.Network/virtualNetworks/subnets\", \"name\": \"vnet-001/subnet-001\", \"dependsOn\": [ \"vnet-001\" ] }"), symbolicName: "vnet-001/subnet-001"),
+            };
 
-            actual = resources[3];
-            Assert.Equal("vnet-001/subnet-001", actual.Value["name"].Value<string>());
+            var actual = context.SortDependencies(resources).Select(r =>
+            {
+                return r.Value["name"].Value<string>();
+            }).ToArray();
+
+            Assert.Equal(
+            [
+                "rt-001",
+                "rt-002",
+                "vnet-001",
+                "vnet-001/subnet-001"
+            ], actual);
         }
 
         #region Helper methods
 
-        private static IResourceValue GetResourceValue(TemplateContext context, JObject resource, string symbolicName = null)
+        private static IResourceValue GetResourceValue(TemplateContext context, JObject resource, string symbolicName = null, int? copyInstance = null)
         {
+            var copy = copyInstance == null || symbolicName == null ? null : new TemplateContext.CopyIndexState
+            {
+                Name = symbolicName,
+                Index = copyInstance.Value
+            };
+
             resource.TryGetProperty("name", out var name);
             resource.TryGetProperty("type", out var type);
             resource.TryGetDependencies(out var dependencies);
             var resourceId = ResourceHelper.CombineResourceId(context.Subscription.SubscriptionId, context.ResourceGroup.Name, type, name);
-            var result = new ResourceValue(resourceId, name, type, symbolicName, resource, null);
+            var result = new ResourceValue(resourceId, name, type, symbolicName, resource, copy);
             context.TrackDependencies(result, dependencies);
             return result;
         }
