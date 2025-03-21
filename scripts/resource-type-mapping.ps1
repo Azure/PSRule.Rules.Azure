5bb51f2fa9d434424dec44f140a1cc1af1fa3195
Bernie White <bewhite@microsoft.com>
2025-03-22 00:57:27 +1000

    Added displayName to mapping (#3291)

--- scripts/resource-type-mapping.ps1
+++ scripts/resource-type-mapping.ps1
@@ -8,15 +8,16 @@ $rules = (Get-PSRule -Module PSRule.Rules.Azure -Baseline Azure.All | ForEach-Ob
     if ($_.Info.Annotations['resourceType'] -ne $null) {
 
         foreach ($resourceType in $_.Info.Annotations['resourceType'].Split(',')) {
-            @{
+            [PSCustomObject]@{
                 severity = $_.Info.Annotations['severity'];
                 pillar = $_.Info.Annotations['pillar'];
                 category = $_.Info.Annotations['category'];
                 resourceType = $resourceType;
                 ruleId = $_.Name;
+                displayName = $_.Info.DisplayName;
             }
         }
     }
 })
 
-$rules | ConvertTo-Json -Depth 100 | Set-Content -Path data/resource-type-mapping.json
+$rules | Sort-Object ruleId,resourceType | ConvertTo-Json -Depth 100 | Set-Content -Path data/resource-type-mapping.json
