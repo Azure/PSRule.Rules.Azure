afa319a88f20c5ba66bc1222d58c959e8600c8cd
Bernie White <bewhite@microsoft.com>
2025-10-04 22:39:05 +1000

    Fix linting

--- src/PSRule.Rules.Azure.Patterns/PatternInfo.cs
+++ src/PSRule.Rules.Azure.Patterns/PatternInfo.cs
@@ -3,9 +3,18 @@
 
 namespace PSRule.Rules.Azure.Patterns;
 
+/// <summary>
+/// Provides information about a pattern.
+/// </summary>
 public sealed class PatternInfo
 {
+    /// <summary>
+    /// Gets or sets the name of the pattern.
+    /// </summary>
     public string Name { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Gets or sets the description of the pattern.
+    /// </summary>
     public string Description { get; set; } = string.Empty;
 }
--- src/PSRule.Rules.Azure.Patterns/Remotes/ContainerRegistry/OciDescriptor.cs
+++ src/PSRule.Rules.Azure.Patterns/Remotes/ContainerRegistry/OciDescriptor.cs
@@ -6,8 +6,18 @@ using System.Text.Json.Serialization;
 
 namespace PSRule.Rules.Azure.Patterns.Remotes.ContainerRegistry;
 
+/// <summary>
+/// Represents an OCI descriptor for container registry artifacts.
+/// </summary>
 public class OciDescriptor
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OciDescriptor"/> class.
+    /// </summary>
+    /// <param name="digest">The digest of the artifact.</param>
+    /// <param name="size">The size of the artifact.</param>
+    /// <param name="mediaType">The media type of the artifact.</param>
+    /// <param name="annotations">Optional annotations for the artifact.</param>
     public OciDescriptor(string digest, long size, string mediaType, IDictionary<string, string>? annotations = null)
     {
         MediaType = mediaType;
@@ -16,6 +26,13 @@ public class OciDescriptor
         Size = size;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OciDescriptor"/> class with JSON constructor.
+    /// </summary>
+    /// <param name="mediaType">The media type of the artifact.</param>
+    /// <param name="digest">The digest of the artifact.</param>
+    /// <param name="size">The size of the artifact.</param>
+    /// <param name="annotations">Optional annotations for the artifact.</param>
     [JsonConstructor]
     public OciDescriptor(string mediaType, string digest, long size, ImmutableDictionary<string, string>? annotations)
     {
@@ -25,8 +42,23 @@ public class OciDescriptor
         Annotations = annotations ?? ImmutableDictionary<string, string>.Empty;
     }
 
+    /// <summary>
+    /// Gets the media type of the artifact.
+    /// </summary>
     public string MediaType { get; }
+
+    /// <summary>
+    /// Gets the digest of the artifact.
+    /// </summary>
     public string Digest { get; }
+
+    /// <summary>
+    /// Gets the size of the artifact.
+    /// </summary>
     public long Size { get; }
+
+    /// <summary>
+    /// Gets the annotations for the artifact.
+    /// </summary>
     public ImmutableDictionary<string, string> Annotations { get; }
 }
