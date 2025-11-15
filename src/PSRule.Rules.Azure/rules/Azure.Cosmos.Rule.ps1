4a76640697a09850a4d640b95097a12e78f8adfa
Benjamin Engeset <benjaminengeset@outlook.com>
2025-11-15 08:55:56 +0100

    remove helper function

--- src/PSRule.Rules.Azure/rules/Azure.Cosmos.Rule.ps1
+++ src/PSRule.Rules.Azure/rules/Azure.Cosmos.Rule.ps1
@@ -20,12 +20,41 @@ Rule 'Azure.Cosmos.DisableLocalAuth' -Ref 'AZR-000420' -Type 'Microsoft.Document
 
 # Synopsis: Use zone redundant Cosmos DB accounts in supported regions to improve reliability.
 Rule 'Azure.Cosmos.AvailabilityZone' -Ref 'AZR-000502' -Type 'Microsoft.DocumentDb/databaseAccounts' -Tag @{ release = 'GA'; ruleSet = '2025_12'; 'Azure.WAF/pillar' = 'Reliability'; } -Labels @{ 'Azure.WAF/maturity' = 'L1' } {
-    Test-CosmosAvailabilityZone
+    # Check for availability zones based on virtual machine scale sets, because it is not exposed through the provider for Cosmos DB.
+    $provider = [PSRule.Rules.Azure.Runtime.Helper]::GetResourceType('Microsoft.Compute', 'virtualMachineScaleSets');
+
+    $Assert.GreaterOrEqual($TargetObject, 'properties.locations', 1);
+
+    if ($TargetObject.properties.locations) {
+        foreach ($location in $TargetObject.properties.locations) {
+            $availabilityZones = GetAvailabilityZone -Location $location.locationName -Zone $provider.ZoneMappings;
+
+            # If the location supports availability zones, ensure zone redundancy is enabled
+            if ($availabilityZones) {
+                $Assert.HasFieldValue($location, 'isZoneRedundant', $true).
+                ReasonFrom('properties.locations', $LocalizedData.CosmosDBAvailabilityZone, $TargetObject.Name, $location.locationName);
+            }
+        }
+    }
 }
 
 # Synopsis: Use zone redundant Cosmos DB vCore clusters in supported regions to improve reliability.
 Rule 'Azure.Cosmos.MongoAvailabilityZone' -Ref 'AZR-000503' -Type 'Microsoft.DocumentDB/mongoClusters' -Tag @{ release = 'GA'; ruleSet = '2025_12'; 'Azure.WAF/pillar' = 'Reliability'; } -Labels @{ 'Azure.WAF/maturity' = 'L1' } {
-    Test-CosmosAvailabilityZone
+    # Check for availability zones based on virtual machine scale sets, because it is not exposed through the provider for Cosmos DB.
+    $provider = [PSRule.Rules.Azure.Runtime.Helper]::GetResourceType('Microsoft.Compute', 'virtualMachineScaleSets');
+    
+    $location = $TargetObject.Location;
+    $availabilityZones = GetAvailabilityZone -Location $location -Zone $provider.ZoneMappings;
+
+    # If the location supports availability zones, ensure zone redundancy is enabled
+    if ($availabilityZones) {
+        $Assert.HasFieldValue($TargetObject, 'properties.highAvailability.targetMode', 'ZoneRedundantPreferred').
+        ReasonFrom('properties.highAvailability.targetMode', $LocalizedData.MongoDBvCoreAvailabilityZone, $TargetObject.Name, $location);
+    }
+    else {
+        # Pass if the region does not support availability zones
+        $Assert.Pass();
+    }
 }
 #endregion Rules
 
@@ -42,46 +71,6 @@ function global:Test-IsNoSQL {
         return $true
     }
     $TargetObject.properties.capabilities.Where({ $_.name -in @('EnableTable', 'EnableCassandra', 'EnableGremlin') }, 'First').Count -eq 0
-}
-
-function global:Test-CosmosAvailabilityZone {
-    [CmdletBinding()]
-    param ()
-
-    # Check for availability zones based on virtual machine scale sets, because it is not exposed through the provider for Cosmos DB.
-    $provider = [PSRule.Rules.Azure.Runtime.Helper]::GetResourceType('Microsoft.Compute', 'virtualMachineScaleSets');
-
-    if ($PSRule.TargetType -eq 'Microsoft.DocumentDb/databaseAccounts') {
-        # For Cosmos DB accounts, check locations
-        $Assert.GreaterOrEqual($TargetObject, 'properties.locations', 1);
-
-        if ($TargetObject.properties.locations) {
-            foreach ($location in $TargetObject.properties.locations) {
-                $availabilityZones = GetAvailabilityZone -Location $location.locationName -Zone $provider.ZoneMappings;
-
-                # If the location supports availability zones, ensure zone redundancy is enabled
-                if ($availabilityZones) {
-                    $Assert.HasFieldValue($location, 'isZoneRedundant', $true).
-                        ReasonFrom('properties.locations', $LocalizedData.CosmosDBAvailabilityZone, $TargetObject.Name, $location.locationName);
-                }
-            }
-        }
-    }
-    elseif ($PSRule.TargetType -eq 'Microsoft.DocumentDB/mongoClusters') {
-        # For MongoDB vCore clusters, check highAvailability.targetMode
-        $location = $TargetObject.Location;
-        $availabilityZones = GetAvailabilityZone -Location $location -Zone $provider.ZoneMappings;
-
-        # If the location supports availability zones, ensure zone redundancy is enabled
-        if ($availabilityZones) {
-            $Assert.HasFieldValue($TargetObject, 'properties.highAvailability.targetMode', 'ZoneRedundantPreferred').
-                ReasonFrom('properties.highAvailability.targetMode', $LocalizedData.CosmosDBClusterAvailabilityZone, $TargetObject.Name, $location);
-        }
-        else {
-            # Pass if the region does not support availability zones
-            $Assert.Pass();
-        }
-    }
 }
 
 #endregion Helper functions
